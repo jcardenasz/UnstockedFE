28c6c0fc5805fccea90127eb8b4996ed72d38d5e
Sagonzalezn <sagonzalezn@unal.edu.co>
2023-10-14 22:48:03 -0500

    Product View

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,17 +2,14 @@ import React from 'react'
 import Navbar from '../components/molecules/navbar/Navbar'
 import Footer from '../components/molecules/footer/Footer'
 import SingleBanner from '../components/molecules/banner/singleBanner'
-/* 
-async function getProducts() {
-    const res = await.fetch('mongodb+srv://samuel:1234@cluster0.ymfpquo.mongodb.net/test?retryWrites=true&w=majority')
-} */
+import ProductSidebar from '../components/molecules/productView/ProductSideBar'
 
-export default function Products() {
+export default function Products(): React.JSX.Element {
     return (
         <div>
             <Navbar />
             <SingleBanner heading="Productos" />
-            <h1></h1>
+            <ProductSidebar />
             <Footer />
         </div>
     )
