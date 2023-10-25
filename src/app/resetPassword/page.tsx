5765ad3bfe4098ca8ef75a87deee5cf2952be49a
Juan Camilo Cardenas Zabala <jcardenasz@unal.edu.co>
2023-10-25 10:48:17 -0500

    reset password page nf, redirect-login. login css

--- src/app/resetPassword/page.tsx
+++ src/app/resetPassword/page.tsx
@@ -1,3 +1,21 @@
 'use client'
-// import React from 'react'
+import React from 'react'
+import ResetPasswordForm from '../components/molecules/resetPassForm/resetPassForm'
+// import Link from "next/link"
+import './resetPassword.css'
+import SingleBanner from '../components/molecules/banner/singleBanner'
+import Footer from '../components/molecules/footer/Footer'
+import Navbar from '../components/molecules/navbar/Navbar'
 
+export default function ResetUserPassword(): JSX.Element {
+    return (
+        <div>
+            <Navbar />
+            <SingleBanner heading="reset your Password" />
+            <div className="container">
+                <ResetPasswordForm />
+            </div>
+            <Footer />
+        </div>
+    )
+}
