32cdc81eff8dacaae9681e3cf65284d21d60079f
Jesus Quinones <jesus.quinones@publicisgroupe.com>
2023-11-10 16:06:45 -0500

    creating form for business

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -10,7 +10,29 @@ import React from 'react';
 export default function Dashboard(): JSX.Element {
 
     return (
-        <>
-        </>
+        <div>
+            <section>
+                <form>
+                    <fieldset>
+                        <label>Business Name</label>
+                        <input type='text' placeholder='Name of your business' />
+                        <label>Business Type</label>
+                        <input type='text' placeholder='What do your business do?' />
+                        <label>Address</label>
+                        <input type='text' placeholder='Where is your office' />
+                        <label>City</label>
+                        <select>
+                            <option>City 1</option>
+                            <option>City 2</option>
+                            <option>City 3</option>
+                            <option>City 4</option>
+                        </select>
+                        <label>Email</label>
+                        <input type='email' placeholder='example@example.com' />
+                        <button>Save Changes</button>
+                    </fieldset>
+                </form>
+            </section>
+        </div>
     )
 }
