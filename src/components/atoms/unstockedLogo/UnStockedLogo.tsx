15c9ae1b3933be1f885fec2b6cb078278d6c600e
Juan Camilo Cardenas Zabala <jcardenasz@unal.edu.co>
2023-11-10 10:36:06 -0500

    pink UNstocked logo

--- src/components/atoms/unstockedLogo/UnStockedLogo.tsx
+++ src/components/atoms/unstockedLogo/UnStockedLogo.tsx
@@ -7,7 +7,7 @@ export default function UnStockedLogo(props: { textColor?: string }): JSX.Elemen
             <div style={{ fontSize: '30px', color: props.textColor}} className="font-bold">
                 <span
                     style={{ fontSize: '35px' }}
-                    className="text-purple-600 font-extrabold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]"
+                    className="text-pink-600 font-extrabold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]"
                 >
                     UN
                 </span>
