4c5342e4263c4fe89a2a9b48250a23c7e9685df5
Juan Camilo Cardenas Zabala <jcardenasz@unal.edu.co>
2023-11-23 23:28:05 -0500

    vercel url added login service

--- src/services/login.service.ts
+++ src/services/login.service.ts
@@ -1,7 +1,8 @@
 import Swal from 'sweetalert2'
 
 export async function loginUser(email: string, password: string): Promise<{status: number, data: any}> {
-    const response = await fetch('http://localhost:4000/api/login', {
+    // const response = await fetch('http://localhost:4000/api/login', {
+    const response = await fetch('https://unstocked-fe.vercel.app/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
