2da3a494baf05fe2ab8fda687e74583ddc9a7b44
Juan Camilo Cardenas Zabala <jcardenasz@unal.edu.co>
2023-11-25 15:48:10 -0500

    eslint fixed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,31 @@
-module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    extends: [
-        'standard-with-typescript',
-        'plugin:react/recommended',
-        'prettier',
-    ],
-    overrides: [
-        {
-            env: {
-                node: true,
-            },
-            files: ['.eslintrc.{js,cjs}'],
-            parserOptions: {
-                sourceType: 'script',
-            },
+export const env = {
+    browser: true,
+    es2021: true,
+};
+export const extend = [
+    'standard-with-typescript',
+    'plugin:react/recommended',
+    'prettier',
+];
+export const overrides = [
+    {
+        env: {
+            node: true,
         },
-    ],
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-    },
-    plugins: ['react'],
-    rules: {},
-    settings: {
-        react: {
-            version: 'latest',
+        files: ['.eslintrc.{js,cjs}'],
+        parserOptions: {
+            sourceType: 'script',
         },
     },
-}
+];
+export const parserOptions = {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+};
+export const plugins = ['react'];
+export const rules = {};
+export const settings = {
+    react: {
+        version: 'detect',
+    },
+};
