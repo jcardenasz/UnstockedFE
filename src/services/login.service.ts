1a1195f65ce6f796f1a90b8329197c1a30709fa5
Juan Camilo Cardenas Zabala <jcardenasz@unal.edu.co>
2023-11-25 16:24:19 -0500

    login authorization change, working

--- src/services/login.service.ts
+++ src/services/login.service.ts
@@ -2,13 +2,22 @@ import Swal from 'sweetalert2'
 import { type SignInResponse, signIn } from 'next-auth/react'
 
 export async function loginUser(email: string, password: string): Promise<SignInResponse> {
+    
 
     const responseNextAuth = await signIn("credentials", {
         email,
         password,
         redirect: false
     })
-
+    await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/login`, {
+        // const response = await fetch("http://u-nstocked-bmb5.vercel.app/api/login", {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+    })
     if ((responseNextAuth?.error) != null) {
         void Swal.fire({
             icon: 'error',
