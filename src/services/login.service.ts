bb4a6d8591a4d73ce8025ac853dd75ee1a13e8cb
Juan Camilo Cardenas Zabala <jcardenasz@unal.edu.co>
2023-11-24 22:42:47 -0500

    cookies visible and login fetch json correct

--- src/services/login.service.ts
+++ src/services/login.service.ts
@@ -2,15 +2,19 @@ import Swal from 'sweetalert2'
 
 export async function loginUser(email: string, password: string): Promise<{status: number, data: any}> {
     console.log("Backend URL: ", process.env.NEXT_PUBLIC_BACKEND_URL);
-     const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/login`, {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/login`, {
     // const response = await fetch("http://u-nstocked-bmb5.vercel.app/api/login", {
         method: 'POST',
+        credentials: 'include',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({ email, password })
     })
+    console.log("Response: ", response);
+    console.log("Response status: ", response.status);
     const data = await response.json()
+    console.log("Response data: ", data);
 
     if (!response.ok) {
         void Swal.fire({
